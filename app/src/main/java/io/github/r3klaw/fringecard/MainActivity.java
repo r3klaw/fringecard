836aa4bad25bd542c6e201d2f7a9fbffc26b6bf7
[r3klaw] <[rashid.david12@gmail.com]>
2018-01-09 12:47:49 +0300

    Initial commit

---
+++ app/src/main/java/io/github/r3klaw/fringecard/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.r3klaw.fringecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
